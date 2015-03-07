f61f1aa0c1d38c93875b6c4a261a3de5efe8c4cd
lukas <resch.lukas10@gmail.com>
2015-03-06 22:36:47 -0800

    (Noch) Einfacher Gegner!

--- src/klassen/enemys/StandartEnemy.java
+++ src/klassen/enemys/StandartEnemy.java
@@ -1,5 +1,6 @@
 package klassen.enemys;
 
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 import klassen.player.Player;
@@ -10,9 +11,24 @@ public class StandartEnemy extends Enemy{
     public StandartEnemy(float x, float y, int speed, LinkedList<PlayerSpritzer> playerSpritzers, Player player, Rectangle bounding) {
         super(x, y, speed, playerSpritzers, player, bounding);
         
-        
-        
+        super.speedX=speedX;
+        super.speedY=speedY;
+        setColor(Color.blue);
+        setKnockback(true);
     }
     
+    @Override
+    public void update(float tslf)
+    {
+        if(collidePlayerSpritzer())
+        {
+          live-=10;
+        }
     
+        Rectangle rect=player.getBounding();
+        rect.x-=Player.speedX;
+        rect.y-=Player.speedY;
+        rebound(rect);
+        super.update(tslf);
+    }
 }

a97a5cb6ff26b8a121e6cd7e665cb23253e362b1
lukas <resch.lukas10@gmail.com>
2015-03-06 16:36:36 -0800

    Hallo!

--- src/klassen/enemys/BasicEnemy.java
+++ src/klassen/enemys/BasicEnemy.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package klassen.enemys;
 
 import java.awt.Color;
@@ -11,10 +6,6 @@ import java.util.LinkedList;
 import klassen.player.Player;
 import klassen.player.PlayerSpritzer;
 
-/**
- *
- * @author Christian
- */
 public class BasicEnemy extends Enemy
 {
   public BasicEnemy(float x, float y, int speed, int speedX, int speedY,LinkedList<PlayerSpritzer> playerSpritzers,Player player)

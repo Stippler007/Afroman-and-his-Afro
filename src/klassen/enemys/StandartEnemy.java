f965f4fd25da7109e143d11836cbf26be61a1a39
lukas <resch.lukas10@gmail.com>
2015-03-16 19:59:36 -0700

    Enemie verbessert

--- src/klassen/enemys/StandartEnemy.java
+++ src/klassen/enemys/StandartEnemy.java
@@ -3,10 +3,12 @@ package klassen.enemys;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import klassen.player.Player;
 import klassen.player.PlayerSpritzer;
 
-public class StandartEnemy extends Enemy{
+public class StandartEnemy extends Enemy implements Runnable{
 
     LinkedList<EnemySpritzer> enemySpritzer;
     
@@ -18,6 +20,7 @@ public class StandartEnemy extends Enemy{
         setColor(Color.blue);
         setKnockback(true);
         this.enemySpritzer = enemySpritzer;
+        new Thread(this).start();
     }
     
     @Override
@@ -26,17 +29,48 @@ public class StandartEnemy extends Enemy{
         if(collidePlayerSpritzer())
         {
           live-=10;
-          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, speed*24,0));
-          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, -speed*24,0));
-          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, 0, speed*24));
-          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, 0,-speed*24));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, speed*3,0));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, -speed*3,0));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, 0, speed*3));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, 0,-speed*3));
           
         }
+        
+        speedX = 0;
+        speedY = 0;
     
         Rectangle rect=player.getBounding();
         rect.x-=Player.speedX;
         rect.y-=Player.speedY;
+        
+        
+        moveZiel(player.getX()+player.getBounding().width/2, player.getY()+player.getBounding().height/2,speed);
         rebound(rect);
         super.update(tslf);
+    }
+    
+    public void run()
+    {
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(StandartEnemy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        while(live > 0)
+        {
+            
+            
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, speed*3,0));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, -speed*3,0));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, 0, speed*3));
+          enemySpritzer.add(new EnemySpritzer(x+bounding.width/2-7, y+bounding.width/2-7, 0,-speed*3));
+          
+            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(StandartEnemy.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }

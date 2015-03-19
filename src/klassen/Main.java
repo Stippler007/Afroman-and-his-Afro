7466571a65f0da402cd59cd1daf7c6a636cbdc59
lukas <resch.lukas10@gmail.com>
2015-03-18 17:26:18 -0700

    Standart Enemy angepasst

--- src/klassen/Main.java
+++ src/klassen/Main.java
@@ -35,7 +35,7 @@ public class Main
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     f.setLocationRelativeTo(null);
     
-    enemys.add(new StandartEnemy(300, 300, 20, 20, 0, playerSpritzers,enemys, player, enemySpritzerses));
+    enemys.add(new StandartEnemy(300, 300, 20, 0, 0, playerSpritzers,enemys, player, enemySpritzerses));
     enemys.add(new BasicEnemy(300, 300, 30, 20, 0, playerSpritzers, enemys, player));
     enemys.add(new BasicEnemy(300, 300, 30, 20, 0, playerSpritzers, enemys, player));
     

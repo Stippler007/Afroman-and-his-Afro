d5b8306c62a76f3f06f49b9f82ab5eca3aca9b38
lukas <resch.lukas10@gmail.com>
2015-03-18 21:45:16 -0700

    irgendwas

--- src/klassen/listener/MML.java
+++ src/klassen/listener/MML.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package klassen.listener;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-/**
- *
- * @author Christian
- */
 public class MML implements MouseMotionListener
 {
   public static int x;

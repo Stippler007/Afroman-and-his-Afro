48b02eccad8a34f83a50e2aec084f5a24755ca43
lukas <resch.lukas10@gmail.com>
2015-03-18 17:26:48 -0700

    nichts besonderes

--- src/klassen/player/Player.java
+++ src/klassen/player/Player.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package klassen.player;
 
 import java.awt.Rectangle;
@@ -13,10 +8,6 @@ import klassen.enemys.EnemySpritzer;
 import klassen.karte.GameObject;
 import klassen.listener.KL;
 
-/**
- *
- * @author Christian
- */
 public class Player
 {
   private float x;

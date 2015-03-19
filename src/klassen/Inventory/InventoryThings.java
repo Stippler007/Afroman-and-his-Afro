8572720880d7e8bbbf6b8e1f6cda83e6a10ba893
lukas <resch.lukas10@gmail.com>
2015-03-19 16:01:47 -0700

    Inventory

---
+++ src/klassen/Inventory/InventoryThings.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klassen.Inventory;
+
+import klassen.karte.GameObject;
+
+/**
+ *
+ * @author lukas
+ */
+public class InventoryThings {
+    
+    private GameObject[][] map;
+    
+    public InventoryThings(InventoryDraw iv)
+    {
+        
+    }
+    
+    public void setMap(GameObject[][] map)
+    {
+        this.map = map;
+    }
+    
+}

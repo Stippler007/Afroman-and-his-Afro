666f1780f2f41b745efe893fbc0feda1cc0e4a43
lukas <resch.lukas10@gmail.com>
2015-03-21 02:31:04 -0700

    Shop fast fertig!

--- src/klassen/Shop/ShopGUI.java
+++ src/klassen/Shop/ShopGUI.java
@@ -1,54 +1,142 @@
 package klassen.Shop;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
+import java.util.LinkedList;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import klassen.ImageFactory;
+import klassen.Inventory.InventoryDraw;
 
 /**
  *
  * @author lukas
  */
-public class ShopGUI extends JPanel implements AdjustmentListener{
+public class ShopGUI extends JPanel implements AdjustmentListener, ActionListener{
     
     private JFrame frame;
     private JScrollBar scrollBar;
+    private int money = 0;
+    private JLabel moneyTxt;
+    private JPanel panel;
+    
+    private int toTheTop = 0;
+    
+    private LinkedList<Integer> strongness = new LinkedList<>();
+    private LinkedList<Integer> radius = new LinkedList<>();
+    private LinkedList<JPanel> mainPanel = new LinkedList<>();
+    private LinkedList<JLabel> pictures = new LinkedList<>();
+    private LinkedList<JPanel> underPanel = new LinkedList<>();
+    private LinkedList<JButton> btn = new LinkedList<>();
+    private LinkedList<Integer> state = new LinkedList<>();
+    private LinkedList<Integer> cost = new LinkedList<>();
     
     public ShopGUI()
     {
         frame = new JFrame("Shop");
         frame.setSize(400,400);
+        frame.setLayout(new BorderLayout());
         
-        scrollBar = new JScrollBar();
+        scrollBar = new JScrollBar(JScrollBar.VERTICAL,0,200,0,1000);
         scrollBar.addAdjustmentListener(this);
-        scrollBar.setValue(2);
-        scrollBar.setMaximum(20);
         
-//        System.out.println(scrollBar.getMaximum());
+        moneyTxt = new JLabel("Money: "+  money);
+        moneyTxt.setHorizontalAlignment(JLabel.CENTER);
+        moneyTxt.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, new java.awt.Color(210,180,140)));
+        frame.add(moneyTxt,BorderLayout.NORTH);
         
-        this.setLayout(new BorderLayout());
-        this.add(scrollBar, BorderLayout.EAST);
+        this.setLayout(null);
         
-        frame.add(this);
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(scrollBar, BorderLayout.EAST);
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.add(this, BorderLayout.CENTER);
+        
+        frame.add(panel);
+        
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        
+        addShopItem("Stone", InventoryDraw.stone, 0,0,20);
+        addShopItem("Stone", InventoryDraw.stone, 0,0,20);
+        addShopItem("Stone", InventoryDraw.stone, 0,0,20);
+        addShopItem("Stone", InventoryDraw.stone, 0,0,20);
+        addShopItem("Tower", InventoryDraw.tower1, 50,100,20);
+        addShopItem("Tower", InventoryDraw.tower1, 50,100,20);
+        addShopItem("Tower", InventoryDraw.tower1, 50,100,20);
+        addShopItem("Tower", InventoryDraw.tower1, 50,100,20);
         
     }
     public void setFrameVisible(boolean visible)
     {
         frame.setVisible(visible);
     }
+    public void addShopItem(String name, int state, int strongness, int radius, int cost)
+    {
+        if(state == InventoryDraw.stone) pictures.add(new JLabel(new ImageIcon(ImageFactory.getImageFactory().getLooks("rock0"))));
+        else if(state == InventoryDraw.tower1) pictures.add(new JLabel(new ImageIcon(ImageFactory.getImageFactory().getLooks("BasicTower"))));
+        else return;
+        
+        this.state.add(state);
+        this.strongness.add(strongness);
+        this.radius.add(radius);
+        this.cost.add(cost);
+        
+        mainPanel.add(new JPanel());
+        underPanel.add(new JPanel());
+        
+        underPanel.get(underPanel.size()-1).setLayout(new GridLayout(2,1));
+        underPanel.get(underPanel.size()-1).add(new JLabel("Damage: " + strongness));
+        underPanel.get(underPanel.size()-1).add(new JLabel("Radius: " + strongness));
+        
+        btn.add(new JButton(cost+"Y"));
+        btn.get(btn.size()-1).addActionListener(this);
+        
+        mainPanel.get(mainPanel.size()-1).setLayout(new GridLayout(1,3));
+        mainPanel.get(mainPanel.size()-1).setBorder(BorderFactory.createTitledBorder(name));
+        mainPanel.get(mainPanel.size()-1).add(pictures.get(pictures.size()-1));
+        mainPanel.get(mainPanel.size()-1).add(underPanel.get(underPanel.size()-1));
+        mainPanel.get(mainPanel.size()-1).add(btn.get(btn.size()-1));
+        
+        drawThings();
+    }
+    
+    public void drawThings()
+    {
+        for (int i = 0; i < mainPanel.size(); i++) {
+            
+            mainPanel.get(i).setBounds(0,(i*100)-toTheTop,getWidth(), 100);
+            
+            this.add(mainPanel.get(i));
+            
+        }
+        
+        scrollBar.setVisibleAmount(1000-((100*mainPanel.size())-(getHeight())));
+    }
 
     @Override
     public void adjustmentValueChanged(AdjustmentEvent e) {
-        System.out.println(e.getValue());
+        toTheTop = e.getValue();
+        drawThings();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
     }
     
 }
